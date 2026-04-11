1dcde18dd8bc4278ae3df881d6c96aefa7472cf4
dev-gust <gust.alves.rodrigues@outlook.com>
2026-04-11 20:46:25 -0300

    feat: add player

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
+    clock = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         log_state()
 
@@ -14,7 +17,9 @@ def main():
                 return
 
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
